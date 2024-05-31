3b84bcb53084395d9398f6de2742c9d3d258f6c8
ankitapriya108 <139537048+ankitapriya108@users.noreply.github.com>
2024-05-31 23:34:07 +0530

    added  delete function

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,26 +1,202 @@
 
+// import React, { useState, useEffect } from 'react';
+// import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+// import EditIcon from '@mui/icons-material/Edit';
+// import DeleteIcon from '@mui/icons-material/Delete';
+
+// function Create() {
+//   const [resumeProfiles, setResumeProfiles] = useState([]);
+
+ 
+
+//   useEffect(() => {
+//     const fetchUserDetails = async () => {
+//       try {
+//         const userEmail = sessionStorage.getItem("email");
+
+//         if (!userEmail) {
+//           console.error("Email not found in session storage");
+//           setResumeProfiles([]);
+//           return;
+//         }
+
+//         const response = await axios.get(`http://localhost:8000/userData?email=${userEmail}`);
+//         const { user, resumeProfiles } = response.data;
+//         console.log("Fetched data:", response.data);
+
+//         if (resumeProfiles && resumeProfiles.length > 0) {
+//           setResumeProfiles(resumeProfiles);
+//         } else {
+//           setResumeProfiles([]);
+//         }
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//         setResumeProfiles([]);
+//       }
+//     };
+
+//     fetchUserDetails();
+//   }, []);
+
+ 
+//   const handleDelete = async (id) => {
+//     console.log("id:", id);
+//     try {
+//       await axios.delete(`http://localhost:8000/delete/${id}`);
+//       setResumeProfiles((prevProfiles) =>
+//         prevProfiles.filter((profile) => profile._id !== id)
+//       );
+//     } catch (error) {
+//       console.error("error", error);
+//     }
+//   };
+
+
+//   return (
+//     <div className='mx-[6rem]'>
+//       <Link to='/new'>
+//         <div className='h-[6rem] w-[6rem] border border-solid border-green-600 border-2 text-center pt-3 mt-[5rem]'>
+//           <h4 className='text-black'><AddCircleOutlineIcon /></h4>
+//           <p className='text-black'> Create-new </p>
+//         </div>
+//       </Link>
+
+//       <div className="flex w-[100%] flex-col sm:flex-row justify-center items-center sm:justify-start">
+//         {resumeProfiles.length > 0 ? (
+//           resumeProfiles.map((profile, index) => (
+//             <div key={index} className="flex flex-col gap-y-[.3rem]  p-5 m-5 border-[.2rem] border-slate-400 rounded-lg h-[22rem] w-[19rem] overflow-scroll">
+//               <div className="flex justify-end gap-x-[.5rem] items-center">
+//               {/* <EditIcon onClick={() => handleEdit(profile._id)} /> */}
+//                 <Link to={`/new/MyDetails/${profile._id}`}>
+//                     <EditIcon/>
+//                   </Link>
+//                 <DeleteIcon onClick={() => handleDelete(profile._id)}/>
+//                 </div>
+               
+//               <p className="font-medium">
+//                 Image:
+//                 {profile.mydetails?.[0]?.image && (
+//                   <img src={profile.mydetails[0]?.image} alt="" />
+//                 )}
+//               </p>
+
+//               <p className="font-medium">
+//                 Name:
+//                 <span className="font-normal pl-2">{profile.mydetails?.[0]?.name}</span>
+//               </p>
+//               <p className="font-medium">
+//                 Email:
+//                 <span className="font-normal pl-2">{profile.mydetails?.[0]?.email}</span>
+//               </p>
+              
+//               <p className="font-medium">
+//                 Role:
+//                 <span className="font-normal pl-2">{profile.mydetails?.[0]?.role}</span>
+//               </p>
+//               <p className="font-medium">
+//                 Total Experience:
+//                 <span className="font-normal pl-2">{profile.mydetails?.[0]?.totalExp}</span>
+//               </p>
+
+//               <p className="font-medium">
+//                 About:
+//                 <span className="font-normal pl-2">{profile.aboutme?.[0]?.about}</span>
+//                 <br />
+//                 Points:
+//                 <span className="font-normal pl-2">{profile.aboutme?.[0]?.aboutPoint?.join(",")}</span>
+//               </p>
+              
+//               {/* <p className="font-medium">
+//                   Skills:
+//                   <span className="font-normal  pl-2">
+//                     {profile.data?.join(", ")}
+//                   </span>
+//                 </p> */}
+
+// {profile.skills?.[0]?.data?.map((skill, index) => (
+//   <p className="font-medium" key={index}>
+//     Skills:
+//     <span className="font-normal pl-2">{skill.category}</span> - 
+//     <span className="font-normal pl-2">{skill.skills}</span>
+//   </p>
+// ))}
+
+        
+//               <div className="font-medium">
+//                 Work Experience:
+//                 {profile.work?.[0]?.workExperience?.map((exp, idx) => (
+//                   <div key={idx} className="font-normal flex flex-col gap-y-[.3rem]">
+//                     <p className="font-medium">
+//                       Client Description:
+//                       <span className="font-normal pl-2">{exp.clientDescription}</span>
+//                     </p>
+//                     <p className="font-medium">
+//                       Country:
+//                       <span className="font-normal pl-2">{exp.country}</span>
+//                     </p>
+//                     <p className="font-medium">
+//                       Project Name:
+//                       <span className="font-normal pl-2">{exp.projectName}</span>
+//                     </p>
+//                     <p className="font-medium">
+//                       Role:
+//                       <span className="font-normal pl-2">{exp.roleWork}</span>
+//                     </p>
+//                     <p className="font-medium">
+//                       Start Date:
+//                       <span className="font-normal pl-2">{exp.startDate}</span>
+//                     </p>
+//                     <p className="font-medium">
+//                       End Date:
+//                       <span className="font-normal pl-2">{exp.endDate}</span>
+//                     </p>
+//                     <p className="font-medium">
+//                       Business Solution:
+//                       <span className="font-normal pl-2">{exp.businessSolution}</span>
+//                     </p>
+//                     <p className="font-medium">
+//                       Technology Stack:
+//                       <span className="font-normal pl-2">{exp.technologyStack?.join(", ")}</span>
+//                     </p>
+                    
+//                     <p className="font-medium">
+//                      Responsibilities:
+//                       <span className="font-normal pl-2">{exp.projectResponsibility?.join(", ")}</span>
+//                     </p>
+                    
+//                   </div>
+//                 ))}
+//               </div>
+//             </div>
+//           ))
+//         ) : (
+//           <p className='m-auto pt-[4rem]'>No resume profiles found</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+ 
+// }
+
+// export default Create;
+
+
+
+
+
+// Import necessary hooks and icons
 import React, { useState, useEffect } from 'react';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 function Create() {
   const [resumeProfiles, setResumeProfiles] = useState([]);
-
-  // const [editingProfile, setEditingProfile] = useState(null);
-  // const [formData, setFormData] = useState({
-  //   name: '',
-  //   email:'',
-  //   role: '',
-  //   totalExp: '',
-  //   about:'',
-  //   aboutPoints:'',
-  //   data:'',
-  //   workExperirence:''
-   
-  // });
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserDetails = async () => {
@@ -51,7 +227,6 @@ function Create() {
     fetchUserDetails();
   }, []);
 
- 
   const handleDelete = async (id) => {
     console.log("id:", id);
     try {
@@ -64,69 +239,28 @@ function Create() {
     }
   };
 
-
-
-  // const handleEdit = (profile) => {
-  //   setEditingProfile(profile);
-  //   setFormData({
-  //     name: profile.mydetails?.[0]?.name || '',
-  //     email: profile.mydetails?.[0]?.email || '',
-  //     role: profile.mydetails?.[0]?.role || '',
-  //     totalExp: profile.mydetails?.[0]?.totalExp || '',
-  //     aboutme: profile.aboutme?.[0]?.about|| '',
-  //     aboutme: profile.aboutme?.[0]?.aboutPoints|| '',
-  //     skills: profile.skills?.[0]?.data?.catogory|| '',
-  //     skills: profile.skills?.[0]?.data?.skills|| '',
-  //     work: profile.work?.[0]?.client|| '',
-  //     work: profile.work?.[0]?.country|| '',
-  //     work: profile.work?.[0]?.projectName|| '',
-  //     work: profile.work?.[0]?.roleWork|| '',
-  //     work: profile.work?.[0]?.startDate|| '',
-  //     work: profile.work?.[0]?.endDate|| '',
-  //     work: profile.work?.[0]?.businessSolution|| '',
-  //     work: profile.work?.[0]?.technologyStack|| '',
-  //     work: profile.work?.[0]?.projectResponsibiliy|| '',
-    
-  //   });
-  // };
-
-
-
-  const handleEdit = async (id, index) => {
-    const dataToEdit = resumeProfiles[index];
-    try {
-      await axios.put(`http://localhost:8000/update/${id}`, dataToEdit, {
-       headers: {
-         "Content-Type": "application/json",
-        },
-    });
-   } 
-   catch (error) {
-     console.log(error);
-      }
-   };
+  const handleEdit = (id) => {
+    navigate(`/new/MyDetails/${id}`);
+  };
 
   return (
     <div className='mx-[6rem]'>
       <Link to='/new'>
         <div className='h-[6rem] w-[6rem] border border-solid border-green-600 border-2 text-center pt-3 mt-[5rem]'>
           <h4 className='text-black'><AddCircleOutlineIcon /></h4>
-          <p className='text-black'> Create-new </p>
+          <p className='text-black'>Create-new</p>
         </div>
       </Link>
 
       <div className="flex w-[100%] flex-col sm:flex-row justify-center items-center sm:justify-start">
         {resumeProfiles.length > 0 ? (
           resumeProfiles.map((profile, index) => (
-            <div key={index} className="flex flex-col gap-y-[.3rem]  p-5 m-5 border-[.2rem] border-slate-400 rounded-lg h-[22rem] w-[19rem] overflow-scroll">
+            <div key={index} className="flex flex-col gap-y-[.3rem] p-5 m-5 border-[.2rem] border-slate-400 rounded-lg h-[22rem] w-[19rem] overflow-scroll">
               <div className="flex justify-end gap-x-[.5rem] items-center">
-              <EditIcon onClick={() => handleEdit(profile._id)} />
-                {/* <Link to={`/new/MyDetails/${profile._id}`}>
-                    <EditIcon/>
-                  </Link> */}
-                <DeleteIcon onClick={() => handleDelete(profile._id)}/>
-                </div>
-               
+                <EditIcon onClick={() => handleEdit(profile._id)} />
+                <DeleteIcon onClick={() => handleDelete(profile._id)} />
+              </div>
+
               <p className="font-medium">
                 Image:
                 {profile.mydetails?.[0]?.image && (
@@ -160,22 +294,14 @@ function Create() {
                 <span className="font-normal pl-2">{profile.aboutme?.[0]?.aboutPoint?.join(",")}</span>
               </p>
               
-              {/* <p className="font-medium">
+              {profile.skills?.[0]?.data?.map((skill, index) => (
+                <p className="font-medium" key={index}>
                   Skills:
-                  <span className="font-normal  pl-2">
-                    {profile.data?.join(", ")}
-                  </span>
-                </p> */}
+                  <span className="font-normal pl-2">{skill.category}</span> - 
+                  <span className="font-normal pl-2">{skill.skills}</span>
+                </p>
+              ))}
 
-{profile.skills?.[0]?.data?.map((skill, index) => (
-  <p className="font-medium" key={index}>
-    Skills:
-    <span className="font-normal pl-2">{skill.category}</span> - 
-    <span className="font-normal pl-2">{skill.skills}</span>
-  </p>
-))}
-
-        
               <div className="font-medium">
                 Work Experience:
                 {profile.work?.[0]?.workExperience?.map((exp, idx) => (
@@ -214,10 +340,9 @@ function Create() {
                     </p>
                     
                     <p className="font-medium">
-                     Responsibilities:
+                      Responsibilities:
                       <span className="font-normal pl-2">{exp.projectResponsibility?.join(", ")}</span>
                     </p>
-                    
                   </div>
                 ))}
               </div>
@@ -229,7 +354,7 @@ function Create() {
       </div>
     </div>
   );
- 
 }
 
 export default Create;
+
--- src/Edit.jsx
+++ src/Edit.jsx
@@ -1,105 +1,7 @@
 
-// import React, { useState, useEffect, useContext } from "react";
-// import { Link, Outlet, useParams } from "react-router-dom";
-// import ArrowRightAltIcon from "@mui/icons-material/ArrowCircleRight";
-// import { userData } from "./App";
-// import axios from 'axios';
-
-// function Edit() {
-//   const urlObject = ["mydetails", "aboutme", "skills", "work"];
-//   const [counter, setCounter] = useState(1);
-//   const [nextPage, setNextPage] = useState(urlObject[counter]);
-//   const {id} = useParams();
-
-//   const {name, setName, email, setEmail, role, setRole, totalExp, setTotalExp, about, setAbout, image, setImage, aboutPoint, setAboutPoint, data, setData, workExperience, setWorkExperience} = useContext(userData);
-
-//   const handleNextClick = (e) => {
-//     setCounter(counter + 1);
-//   };
-
-//   // useEffect(() => {
-//   //   setNextPage(urlObject[counter]);
-//   // }, [counter]);
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-
-//     fetch("http://localhost:8000/send", {
-//       method: "post",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         mydetails: {
-//           name: name,
-//           email: email,
-//           role: role,
-//           totalExp: totalExp
-//         },
-//         aboutme: {
-//           about: about,
-//           aboutPoint: aboutPoint,
-//         },
-//         skills: {
-//           data: data
-//         },
-//         work: {
-//           workExperience: workExperience
-//         }
-//       })
-//     })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log("Response:", data);
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-//     console.log(userData);
-//   }
-
-
-
-//   useEffect(()=>{
-//     if(id){
-//       setNextPage(`${urlObject[counter]}/${id}`);
-//     }
-//     else{
-//       setNextPage(urlObject[counter]);
-//     }
-//   },[counter,id]);
-
-
-
- 
-
-//   return (
-//     <>
-//       <div className="resumeSection w-1/2">
-//         <form method="post" onSubmit={handleSubmit}>
-//           <div className="form-group flex justify-end">
-//             <Link
-//               className="next bg-slate-500 text-white rounded px-2 py-2 mt-[1rem]"
-//               to={nextPage}
-//               onClick={handleNextClick}
-//             >
-//               Next <ArrowRightAltIcon />
-//             </Link>
-//             <button type="submit" className="bg-slate-500 text-white rounded mt-[1rem] px-2 ml-2">SAVE </button>
-            
-//           </div>
-//         </form>
-//         <Outlet />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Edit;
-
 
 import React, { useState, useEffect, useContext } from "react";
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowCircleRight";
 import { userData } from "./App";
 import axios from 'axios';
@@ -108,7 +10,6 @@ function Edit() {
   const urlObject = ["mydetails", "aboutme", "skills", "work"];
   const [counter, setCounter] = useState(1);
   const [nextPage, setNextPage] = useState(urlObject[counter]);
-  const {id} = useParams();
 
   const {name, setName, email, setEmail, role, setRole, totalExp, setTotalExp, about, setAbout, image, setImage, aboutPoint, setAboutPoint, data, setData, workExperience, setWorkExperience} = useContext(userData);
 
@@ -116,45 +17,46 @@ function Edit() {
     setCounter(counter + 1);
   };
 
+  useEffect(() => {
+    setNextPage(urlObject[counter]);
+  }, [counter]);
+
   function handleSubmit(e) {
     e.preventDefault();
 
-    const url = id ? `http://localhost:8000/update/${id}` : 'http://localhost:8000/send';
-
-    axios.put(url, {
-      mydetails: {
-        name: name,
-        email: email,
-        role: role,
-        totalExp: totalExp
+    fetch("http://localhost:8000/send", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
       },
-      aboutme: {
-        about: about,
-        aboutPoint: aboutPoint,
-      },
-      skills: {
-        data: data
-      },
-      work: {
-        workExperience: workExperience
-      }
+      body: JSON.stringify({
+        mydetails: {
+          name: name,
+          email: email,
+          role: role,
+          totalExp: totalExp
+        },
+        aboutme: {
+          about: about,
+          aboutPoint: aboutPoint,
+        },
+        skills: {
+          data: data
+        },
+        work: {
+          workExperience: workExperience
+        }
+      })
     })
-    .then((response) => {
-      console.log("Response:", response.data);
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Response:", data);
     })
     .catch((error) => {
       console.error("Error:", error);
     });
+    console.log(userData);
   }
-
-  useEffect(()=>{
-    if(id){
-      setNextPage(`${urlObject[counter]}/${id}`);
-    }
-    else{
-      setNextPage(urlObject[counter]);
-    }
-  },[counter,id]);
 
   return (
     <>
@@ -169,6 +71,7 @@ function Edit() {
               Next <ArrowRightAltIcon />
             </Link>
             <button type="submit" className="bg-slate-500 text-white rounded mt-[1rem] px-2 ml-2">SAVE </button>
+            
           </div>
         </form>
         <Outlet />
